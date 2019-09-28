8d6955b48bb52e8e8b976b653055ec9d5033e85c
ivanv <ivanv@gost-group.com>
2019-09-28 11:17:13 -0700

    issue #9 добавить картинку в студию, добавить историю

--- src/pages/Studio.tsx
+++ src/pages/Studio.tsx
@@ -2,17 +2,28 @@ import React from "react";
 import { Base, BigRow, BigButtonCol } from "../components";
 import { PageTitle } from "../components/PageTitle";
 import { routerStore } from "../store/RouterStore";
+import { Carousel } from "../components/Carousel";
+
+import studi1PNG from "../images/studio/studio.png"
 
 export const Studio = () => {
   return (
     <Base>
       <PageTitle>СТУДИЯ</PageTitle>
+      <Carousel 
+        items={[
+          { src: studi1PNG }
+        ]}
+      />
       <BigRow>
         <BigButtonCol onClick={() => routerStore.push("/leaders")}>
           РУКОВОДСТВО
         </BigButtonCol>
         <BigButtonCol onClick={() => routerStore.push("/teachers")}>
           ПЕДАГОГИ
+        </BigButtonCol>
+        <BigButtonCol>
+          ИСТОРИЯ
         </BigButtonCol>
       </BigRow>
     </Base>
