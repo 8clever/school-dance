3e957af17070e86b3d93348fe3b6d931d8b4e306
Ivan Vityaev <godofluck89@gmail.com>
2019-11-11 17:42:51 +0300

    add small fix to tables

--- src/components/HeaderCalendar.tsx
+++ src/components/HeaderCalendar.tsx
@@ -69,84 +69,86 @@ export const HeaderCalendar = (props: HeaderCalendarProps) => {
             md={4}>
 
             <table className="calendar-mini">
-              <tr>
-                <td colSpan={7}>
-                  <Row>
-                    <Col xs={2}>
-                      <img 
-                        onClick={() => {
-                          const date = moment(props.date).add(-1, "month");
-                          props.onChange(date.toDate());
-                        }}
-                        className="control"
-                        height={15} 
-                        src={leftPNG} 
-                      />
-                    </Col>
-                    <Col xs={8}>
-                      <span className={"text-uppercase"}>
-                        {moment(props.date).locale("ru").format("MMMM, YYYY")}
-                      </span>
-                    </Col>
-                    <Col xs={2}>
-                      <img 
-                        onClick={() => {
-                          const date = moment(props.date).add(1, "month");
-                          props.onChange(date.toDate());
-                        }}
-                        className="control"
-                        height={15} 
-                        src={rightPNG} 
-                      />
-                    </Col>
-                  </Row>
-                </td>
-              </tr>
-              <tr>
-                <td>пн</td>
-                <td>вт</td>
-                <td>ср</td>
-                <td>чт</td>
-                <td>пт</td>
-                <td>сб</td>
-                <td>вс</td>
-              </tr>
-              {
-                month.map((week, idx) => {
-                  
+              <tbody>
+                <tr>
+                  <td colSpan={7}>
+                    <Row>
+                      <Col xs={2}>
+                        <img 
+                          onClick={() => {
+                            const date = moment(props.date).add(-1, "month");
+                            props.onChange(date.toDate());
+                          }}
+                          className="control"
+                          height={15} 
+                          src={leftPNG} 
+                        />
+                      </Col>
+                      <Col xs={8}>
+                        <span className={"text-uppercase"}>
+                          {moment(props.date).locale("ru").format("MMMM, YYYY")}
+                        </span>
+                      </Col>
+                      <Col xs={2}>
+                        <img 
+                          onClick={() => {
+                            const date = moment(props.date).add(1, "month");
+                            props.onChange(date.toDate());
+                          }}
+                          className="control"
+                          height={15} 
+                          src={rightPNG} 
+                        />
+                      </Col>
+                    </Row>
+                  </td>
+                </tr>
+                <tr>
+                  <td>пн</td>
+                  <td>вт</td>
+                  <td>ср</td>
+                  <td>чт</td>
+                  <td>пт</td>
+                  <td>сб</td>
+                  <td>вс</td>
+                </tr>
+                {
+                  month.map((week, idx) => {
+                    
 
-                  return (
-                    <tr className="week" key={idx}>
-                      {
-                        week.map((day, idx) => {
-                          const times = getTimes(day.day.toDate());
-                          let schedules = [];
+                    return (
+                      <tr className="week" key={idx}>
+                        {
+                          week.map((day, idx) => {
+                            const times = getTimes(day.day.toDate());
+                            let schedules = [];
 
-                          times.forEach((t) => {
-                            const _schedules = findSchedulesByTime(t.time, [props.direction]);
-                            if (!_schedules.length) return null;
-                            schedules = _.unionBy(schedules, _schedules, "_id");
-                          });
+                            times.forEach((t) => {
+                              const _schedules = findSchedulesByTime(t.time, [props.direction]);
+                              if (!_schedules.length) return null;
+                              schedules = _.unionBy(schedules, _schedules, "_id");
+                            });
 
-                          const isSameMonth = day.day.isSame(props.date, "month");
-                          const className = [];
+                            const isSameMonth = day.day.isSame(props.date, "month");
+                            const className = [];
 
-                          if (!isSameMonth) className.push("disabled");
-                          if (schedules.length) className.push("active");
+                            if (!isSameMonth) className.push("disabled");
+                            if (schedules.length) className.push("active");
 
-                          return (
-                            <td 
-                              className={className.join(" ")}
-                              key={idx}>
-                              {day.day.format("D")}
-                            </td>
-                          )
-                        })
-                      }
-                    </tr>
-                  )
-                })
-              }
+                            return (
+                              <td 
+                                className={className.join(" ")}
+                                key={idx}>
+                                {day.day.format("D")}
+                              </td>
+                            )
+                          })
+                        }
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
             </table>
           </Col>  
         </Row> : null
