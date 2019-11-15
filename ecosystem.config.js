70cbe8a6e74547818c35c5ac00a45a1bedd51a92
Ivan Vityaev <godofluck89@gmail.com>
2019-11-15 13:32:36 +0300

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,25 +4,5 @@ module.exports = {
       name: "balet-deploy",
       script: "./dist/server/index.js"
     }
-  ],
-  deploy: {
-    "production" : {
-      "key"  : "/home/ivanv/.ssh/balet.pem",
-      "user" : "ubuntu",
-      "host" : "13.48.25.23",
-      "ref"  : "origin/master",
-      "repo" : "https://github.com/8clever/school-dance.git",
-      "path" : "/home/ubuntu/deploy",
-      "post-deploy" : `
-        /home/ubuntu/.nvm/versions/node/v10.17.0/bin/npm install &&
-        /home/ubuntu/.nvm/versions/node/v10.17.0/bin/npm run build &&
-        pm2 startOrRestart ecosystem.json
-      `,
-      "pre-setup": "sudo apt-get install git",
-      "env": {
-        DEVELOPMENT: "",
-        SALT: "s@altyS@lt"
-      }
-    },
-  }
+  ]
 }
