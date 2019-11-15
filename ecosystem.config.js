5294376d1b194e6c3468e1a82c5fce54af3e01cc
Ivan Vityaev <godofluck89@gmail.com>
2019-11-15 13:20:39 +0300

    add production deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: "balet",
+      name: "balet-deploy",
       script: "./dist/server/index.js"
     }
-  ]
+  ],
+  deploy: {
+    "production" : {
+      "key"  : "/home/ivanv/.ssh/balet.pem",
+      "user" : "ubuntu",
+      "host" : "13.48.25.23",
+      "ref"  : "origin/master",
+      "repo" : "https://github.com/8clever/school-dance.git",
+      "path" : "/home/ubuntu/deploy",
+      "post-deploy" : "npm install && npm run build && pm2 startOrRestart ecosystem.json",
+      "pre-setup": "sudo apt-get install git",
+      "env": {
+        DEVELOPMENT: "",
+        SALT: "s@altyS@lt"
+      }
+    },
+  }
 }
