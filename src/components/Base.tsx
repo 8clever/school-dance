145289f27cb55a17a19cefd9ce960dfc9e0d005d
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-03-02 16:56:02 +0300

    add small fix

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { userStore } from "../store/UserStore";
-import { FlexCol } from "./Flex";
 import { observer } from "mobx-react-lite";
 
 interface Props {
@@ -18,25 +17,23 @@ export const Base = observer((props: Props) => {
   return (
     <div 
       style={{ height: "100vh" }}>
-      <FlexCol column justify="between">
-        <div 
-          className="d-md-h-100"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}>
-          <Header />
-          <div style={{
-            position: "relative",
-            height: "100%"
-          }}>
-            {props.children}
-          </div>
+      <div 
+        className="d-md-h-100"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+        }}>
+        <Header />
+        <div style={{
+          position: "relative",
+          height: "100%",
+        }}>
+          {props.children}
         </div>
         <div>
           <Footer />
         </div>
-      </FlexCol>
+      </div>
     </div>
   )
 })
