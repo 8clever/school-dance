e62e034e64bec54fd26bdafd62fcc115a571a48d
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-03-27 23:18:42 +0300

    SDVS-102 remove input search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,44 +2,23 @@ import React from "react";
 import { BigRow, BigCol } from "./Big";
 import { Logo } from "./Logo";
 import { 
-  Input, 
   ButtonGroup, 
-  Button, 
-  InputGroup, 
-  InputGroupText
+  Button
 } from "reactstrap";
 import { FlexCol } from "./Flex";
 import { observer } from "mobx-react-lite";
 import { routerStore } from "../store/RouterStore";
 import { menuStore } from "../store/MenuStore";
 
-import zalupaSVG from "../images/icons/zalupa.svg";
 import menuPNG from "../images/icons/menu.png";
 import closePNG from "../images/icons/close.png";
 import { HeaderMenu } from "./HeaderMenu";
-import { notifStore } from "../store/NotifStore";
 
 const bigColStyle: React.CSSProperties = {
   minHeight: 64
 }
 
 export const Header = observer(() => {
-
-  const [ searchValue, setSearchValue ] = React.useState("");
-
-  const onSearch = () => {
-    if (searchValue.length < 3) {
-      notifStore.addNotif({
-        duration: 5,
-        message: "Минимум 3 символа",
-        title: "Ошибка"
-      });
-      return;
-    }
-
-    routerStore.push(`/search?text=${searchValue}`)
-  }
-
   return (
     <div className="sticky-top bg-white">
       <BigRow>
@@ -64,36 +43,7 @@ export const Header = observer(() => {
         </BigCol>
         <BigCol 
           style={bigColStyle}
-          className="order-1 order-md-0 d-md-block">
-          <FlexCol align="center">
-            <InputGroup 
-              tyle={{ marginTop: 1 }}>
-              <Input
-                onKeyPress={e => {
-                  if (e.key === "Enter") {
-                    onSearch();
-                  }
-                }}
-                className="bg-white"
-                id="input-search"
-                value={searchValue}
-                onChange={e => {
-                  setSearchValue(e.target.value);
-                }}
-              />
-              <InputGroupText 
-                onClick={onSearch}
-                style={{
-                  cursor: "pointer"
-                }}>
-                <img 
-                  height={15}
-                  width={15}
-                  src={zalupaSVG} 
-                />
-              </InputGroupText>
-            </InputGroup>
-          </FlexCol>
+          className="order-1 order-md-0 d-md-block d-none">
         </BigCol>
         <BigCol 
           xs={6}
