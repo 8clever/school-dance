fb692f29c53ca915f4677c38f05bfa0134e37279
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-03-27 14:49:16 +0300

    SDVS-95 Ограничить ширину 1/3 страницы записаться

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Base } from "../components";
 import { Widget } from "../components/Widget";
+import { Row, Col } from "reactstrap";
 
 export const SignUp = () => {
   return (
     <Base>
-      <Widget 
-        elementId="SiteWidgetMoyklass28608"
-        widgetId="2F5CmCKrfHq1caJ0KrWrYYTo9wsSGRf0nI"
-      />
+      <Row>
+        <Col md={{
+          offset: 4,
+          size: 4
+        }}>
+          <Widget 
+            elementId="SiteWidgetMoyklass28608"
+            widgetId="2F5CmCKrfHq1caJ0KrWrYYTo9wsSGRf0nI"
+          />
+        </Col>
+      </Row>
+      
     </Base>
   )
 }
