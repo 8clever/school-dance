aa6aaf571a78b66eda290d6de1c142a0a7316c44
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-04-06 16:35:30 +0300

    SDVS-103 Страница партнеры

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Base, BigRow, BigCol } from "../components";
-import { CSSTransition } from "react-transition-group";
 
 import imgFondDiana from "../images/partners/fond_diana.png";
 import imgСontextWhite from "../images/partners/context_white.png";
@@ -8,8 +7,9 @@ import imgIlimg from "../images/partners/ilim.png";
 import imgAstoria from "../images/partners/astoria.png";
 import imgGollandia from "../images/partners/new_gollandiya.png";
 import imgCinema from "../images/partners/new_cinema.png";
-import imgGost from "../images/partners/gost.png";
-import { isMobile } from "../utils/isMobile";
+import imgBalletMesse from "../images/partners/ballet_messe.png";
+import imgVoss from "../images/partners/voss.jpg";
+
 import { PageBreadcrumbs } from "../components/PageTitle";
 
 interface PartnerButtonProps {
@@ -21,73 +21,50 @@ interface PartnerButtonProps {
 const height = 260;
 
 const PartnerButton = (props: PartnerButtonProps) => {
-  const [ isHover, setHover ] = React.useState(false);
+
+  const image = (
+    <div 
+      style={{
+        background: `url("${props.img}") no-repeat center / contain`
+      }}
+      className="absolute-container m-5 image" 
+    />
+  )
+
+  const text = (
+    <div 
+      className="absolute-container text" style={{
+        fontFamily: "Styled Font",
+        fontSize: 17.5,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textTransform: "uppercase"
+      }}>
+      <div style={{
+        textAlign: "center"
+      }}>
+        {props.title}
+      </div>
+    </div>
+  )
 
   return (
-    <>
-      {
-        isHover ? null :
-        <BigCol 
-          onClick={() => {
-            window.location.href = props.link;
-          }}
-          height={height}
-          onMouseLeave={() => {
-            setHover(false)
-          }}
-          onMouseEnter={() => {
-            if (isMobile()) return;
-            setHover(true)
-          }}>
-          <div 
-            className="absolute-container" style={{
-              fontFamily: "Styled Font",
-              fontSize: 17.5,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              textTransform: "uppercase"
-            }}>
-            <div style={{
-              textAlign: "center"
-            }}>
-              {props.title}
-            </div>
-          </div>
-        </BigCol>
-      }
-      <CSSTransition 
-        key={props.title}
-        timeout={600}
-        classNames="big"
-        unmountOnExit
-        in={isHover}>
-        {
-          isHover ?
-          <BigCol 
-            onClick={() => {
-              window.location.href = props.link;
-            }}
-            onMouseLeave={() => {
-              setHover(false)
-            }}>
-            <div 
-              style={{
-                background: `url("${props.img}") no-repeat center / contain`
-              }}
-              className="absolute-container m-5" 
-            />
-          </BigCol>
-          : <></>
-        }
-      </CSSTransition>
-    </>
+    <BigCol
+      className="partner" 
+      height={height}
+      onClick={() => {
+        window.location.href = props.link;
+      }}>
+      {image}
+      {text}
+    </BigCol>
   )
 }
 
 export const Partners = () => {
   return (
-    <Base>
+    <Base >
 
       <PageBreadcrumbs 
         items={[
@@ -99,19 +76,19 @@ export const Partners = () => {
 
       <BigRow style={{ fontFamily: "Styled Font" }}>
         <PartnerButton 
-          link="http://www.dianavishneva.com/ru/foundation/"
-          title="ФОНД ДИАНЫ ВИШНЁВОЙ"
-          img={imgFondDiana}
-        />
-        <PartnerButton 
-          link="http://www.contextfest.com"
-          title="ФЕСТИВАЛЬ CONTEXT"
-          img={imgСontextWhite}
-        />
-        <PartnerButton 
           link="https://www.ilimgroup.ru"
           title="ГРУППА КОМПАНИЙ ИЛИМ"
           img={imgIlimg}
+        />
+        <PartnerButton 
+          link="https://simplewine.ru/catalog/voda_i_soki/filter/manufacturer-voss/"
+          title="VOSS"
+          img={imgVoss}
+        />
+        <PartnerButton 
+          link="https://balletmesse.com/mc1/"
+          title="Ballet Messe"
+          img={imgBalletMesse}
         />
         <PartnerButton 
           link="https://www.roccofortehotels.com/ru/hotels-and-resorts/hotel-astoria/"
@@ -124,17 +101,20 @@ export const Partners = () => {
           img={imgGollandia}
         />
         <PartnerButton 
+          link="http://www.dianavishneva.com/ru/foundation/"
+          title="ФОНД ДИАНЫ ВИШНЁВОЙ"
+          img={imgFondDiana}
+        />
+        <PartnerButton 
+          link="http://www.contextfest.com"
+          title="ФЕСТИВАЛЬ CONTEXT"
+          img={imgСontextWhite}
+        />
+        <PartnerButton 
           link="https://alexandrinsky.ru"
           title="Новая Сцена Александрийского театра"
           img={imgCinema}
         />
-        <BigCol height={height} className="d-none d-md-block" />
-        <PartnerButton 
-          link="https://gost-group.ru"
-          title="группа компаний GOST"
-          img={imgGost}
-        />
-        <BigCol height={height} className="d-none d-md-block" />
       </BigRow>
     </Base>
   )
