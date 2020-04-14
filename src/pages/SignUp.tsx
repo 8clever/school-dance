1090c3f8dedd2198d4ec91262c6882af1776a4ae
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-04-14 13:58:31 +0300

    SDVS-121 Скролл на странице записаться

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Base } from "../components";
 import { Widget } from "../components/Widget";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Container } from "reactstrap";
 
 export const SignUp = () => {
   return (
     <Base>
-      <Row>
-        <Col md={{
-          offset: 4,
-          size: 4
-        }}>
-          <Widget 
-            elementId="SiteWidgetMoyklass28608"
-            widgetId="2F5CmCKrfHq1caJ0KrWrYYTo9wsSGRf0nI"
-          />
-        </Col>
-      </Row>
-      
+      <Container fluid>
+        <Row>
+          <Col md={{
+            offset: 4,
+            size: 4
+          }}>
+            <Widget 
+              elementId="SiteWidgetMoyklass28608"
+              widgetId="2F5CmCKrfHq1caJ0KrWrYYTo9wsSGRf0nI"
+            />
+          </Col>
+        </Row>
+      </Container>
     </Base>
   )
 }
