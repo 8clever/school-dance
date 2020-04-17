f79cab36c8060cc5ee4599ae5a682db21fce22f3
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-04-17 13:03:01 +0300

    SDVS-123 Pixel

--- src/index.tsx
+++ src/index.tsx
@@ -28,6 +28,15 @@ const App = (
         />
       </div>
     </noscript>
+
+    {/** facebook pixel */}
+    <noscript>
+      <img 
+        height="1" 
+        width="1"
+        src="https://www.facebook.com/tr?id=640518363194829&ev=PageView&noscript=1"
+      />
+    </noscript>
   </>
 )
 
@@ -37,28 +46,25 @@ if (module["hot"]) {
   module["hot"].accept();
 }
 
-interface WindowLocal extends Window {
-  dataLayer?: IArguments[];
-  ym?: (...args) => void;
-}
+(window => {
+  /** disable right click */
+  window.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+  }, false);
 
-const windowLocal: WindowLocal = window;
+  /** google analytics */
+  function gtag(...args) {window.dataLayer.push(arguments);}
+  window.dataLayer = window.dataLayer || [];
+  gtag('js', new Date());
+  gtag('config', 'UA-163129857-1');
+  executeScript("https://www.googletagmanager.com/gtag/js?id=UA-163129857-1");
 
-/** disable right click */
-windowLocal.addEventListener('contextmenu', function (e) {
-  e.preventDefault();
-}, false);
+  /** yandex metrica */
+  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*(new Date() as any);k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  window.ym(61744756, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
 
-/** google analytics */
-function gtag(...args) {
-  windowLocal.dataLayer.push(arguments);
-}
-
-windowLocal.dataLayer = windowLocal.dataLayer || [];
-gtag('js', new Date());
-gtag('config', 'UA-163129857-1');
-executeScript("https://www.googletagmanager.com/gtag/js?id=UA-163129857-1");
-
-/** yandex metrica */
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*(new Date() as any);k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-windowLocal.ym(61744756, "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});
+  /** facebook pixel */
+  (function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)})(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+  window.fbq('init', '640518363194829');
+  window.fbq('track', 'PageView');
+})(window as any)
