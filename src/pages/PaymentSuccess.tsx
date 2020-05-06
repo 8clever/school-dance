33906b21c61ca2a26f3aa7f8123fbd7f226c17ca
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-05-06 16:40:09 +0300

    SDVS-131 add small fix

--- src/pages/PaymentSuccess.tsx
+++ src/pages/PaymentSuccess.tsx
@@ -1,29 +1,16 @@
 import React from "react";
-import { Base } from "../components";
-import { PageBreadcrumbs } from "../components/PageTitle";
 import { routerStore } from "../store/RouterStore";
 
 export const PaymentSuccess = () => {
   return (
-    <Base>
-      <PageBreadcrumbs items={[
-        {
-          title: "Оплата",
-          url: "/payment"
-        },
-        {
-          title: "Успех"
-        }
-      ]} />
-      <div className="p-5">
-        <h3>Оплата прошла успешно.</h3> 
-        Ждем Вас на занятиях! Расписание <a href="#" onClick={e => {
-          e.preventDefault();
-          routerStore.push("/calendar");
-        }}>
-          https://studiocontext.ru/calendar
-        </a>
-      </div>
-    </Base>
+    <div className="p-5">
+      <h3>Оплата прошла успешно.</h3> 
+      Ждем Вас на занятиях! Расписание <a href="#" onClick={e => {
+        e.preventDefault();
+        routerStore.push("/calendar");
+      }}>
+        https://studiocontext.ru/calendar
+      </a>
+    </div>
   )
 }
