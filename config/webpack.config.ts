a76eda2bf5dd5f29fd9ecf4b576d756b782e81f7
Витяев Иван Павлович <ivan.vitiaev@gost-group.com>
2020-07-29 13:26:13 +0300

    add google verification

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -12,6 +12,7 @@ export const config: Configuration = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       meta: {
+        "google-site-verification": "7I157oqzlVrwKudmMfgeY6HEiVNj40BH0GbrM3LS7Ys",
         "yandex-verification": "365ae7796c8ff6cc",
         viewport: "width=device-width, initial-scale=1, shrink-to-fit=no"
       },
